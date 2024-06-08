0a2b0e456a69a509a34e24c6d4cd3083aee42bf4
YasasRajitha <yasas15955@gmail.com>
2024-06-08 19:34:00 +0530

    constants

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 // comment
 // console.log('Hello World');
 
-let name = 'Yasas';
-console.log(name);
+// let name = 'Yasas';
+// console.log(name);
 
 // Cannot be a reserved keyword
 
 
-let firstName;      //This both are different because js is case sensitive
-let FirstName;      //
+// let firstName;      //This both are different because js is case sensitive
+// let FirstName;      //
+
+// let interestRate = 0.3;
+const interestRate = 0.3;   //if u dont need to reassign use const but if u do use let
+interestRate = 1;
+console.log(interestRate);
