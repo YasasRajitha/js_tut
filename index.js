57596182fc935bd88cb7af98a9c759a6b0ae8261
YasasRajitha <yasas15955@gmail.com>
2024-06-08 19:29:55 +0530

    variables

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 // comment
-console.log('Hello World');
+// console.log('Hello World');
+
+let name = 'Yasas';
+console.log(name);
+
+// Cannot be a reserved keyword
+
+
+let firstName;      //This both are different because js is case sensitive
+let FirstName;      //
