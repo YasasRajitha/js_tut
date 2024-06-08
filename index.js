7e1d28883acfdd4eb6d7452c27c7714292870063
YasasRajitha <yasas15955@gmail.com>
2024-06-08 19:39:42 +0530

    primitive types

--- index.js
+++ index.js
@@ -11,6 +11,12 @@
 // let FirstName;      //
 
 // let interestRate = 0.3;
-const interestRate = 0.3;   //if u dont need to reassign use const but if u do use let
-interestRate = 1;
-console.log(interestRate);
+// const interestRate = 0.3;   //if u dont need to reassign use const but if u do use let
+// interestRate = 1;
+// console.log(interestRate);
+
+let name = "Yasas";     //string
+let age = 30;           //integer
+let isApproved = true;  //boolean
+let fName = undefined;
+let lName = null;
